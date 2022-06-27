e9c6549da913d23df00689224729f6263436479c
Vu Tien Long <tllong20002@gmail.com>
2022-06-28 00:54:34 +0700

    firebase gg fb authentication

--- Client/my-app/src/firebase/config.tsx
+++ Client/my-app/src/firebase/config.tsx
@@ -9,13 +9,13 @@ import { FacebookAuthProvider, fetchSignInMethodsForEmail, getAuth, GoogleAuthPr
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDR4elGDvLgEYmMbosrClOmeKTDgxHPGAA",
-  authDomain: "fir-auth-9969f.firebaseapp.com",
-  projectId: "fir-auth-9969f",
-  storageBucket: "fir-auth-9969f.appspot.com",
-  messagingSenderId: "765124148508",
-  appId: "1:765124148508:web:6ee0ab25d585423b0c4ed9",
-  measurementId: "G-YJ4JC8W7JH"
+  apiKey: "AIzaSyAAJ5bOTqXs8O0MCJlkHs3J68zxAqfFwFE",
+  authDomain: "fir-facebook-fdc7d.firebaseapp.com",
+  projectId: "fir-facebook-fdc7d",
+  storageBucket: "fir-facebook-fdc7d.appspot.com",
+  messagingSenderId: "847980349716",
+  appId: "1:847980349716:web:a94a1c7e6f92688e521a01",
+  measurementId: "G-G4DEB0CLRQ"
 };
 
 // Initialize Firebase
@@ -47,69 +47,6 @@ export const logOut= () => {
   }
 };
 
-export const signInWithGoogle =  () => {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-        console.log(user);
-        
-    }else{
-      console.log("unauthorized");
-      
-    }
-  
-})
-   signInWithPopup(auth, googleProvider).then((result) => {
-    // console.log(result);
-    const id = result.user.uid;
-    const name = result.user.displayName;
-    const email = result.user.email;
-    const image = result.user.photoURL;
-    console.log(result.user);
-    
-      localStorage.setItem("user", JSON.stringify({id,name, email, image}));
-   
-    
-  }).then(() => {
-
-      location.href = "/"
-  })
-  .catch((error) => {
-    console.log(error);
-    
-  });
-}
-
-
-export const signInWithFacebook =  () => {
-  // logOut();
-   onAuthStateChanged(auth, (user) => {
-    if (user) {
-        console.log(user);
-        
-    }else{
-      console.log("unauthorized");
-      
-    }
-  
-})
-
-  signInWithPopup( auth, facebookProvider).then((result) => {
-  
-    // const id = result.user.uid;
-    // const name = result.user.displayName;
-    // const email = result.user.email;
-    // const image = result.user.photoURL;
-    console.log(result);
-
-    //   localStorage.setItem("user", JSON.stringify({id,name, email, image}));
-   
-    
-  })
-  .catch((err) => {
-    console.log(err);
-  
-})
-}
 
 
 
